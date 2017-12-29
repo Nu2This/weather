d88bec9da93411d0e9c6831d58bf4a3aa975e011
Michael Heidelberger <mheide@localhost.localdomain>
2017-12-29 15:11:12 -0600

    Using a json file from the Wunderground API to create my own Terminal weather app. Currently reads the file and pulls out the shortened days of the week from the 10 day forcast of days 0-2.

---
+++ weather.py
@@ -0,0 +1,22 @@
+import json
+
+def extended_forecast():
+    with open('/home/mheide/Documents/python/Weather/56119.json')as json_file:
+        #Dig into the nested json to get workable data out of it. 
+        data = json.load(json_file)
+        forecast = data['forecast']
+        simpleforecast = forecast['simpleforecast']
+        forecastday = simpleforecast['forecastday']
+        #Made these to make it easier to remember what is what
+        today = forecastday[0]
+        tomorrow = forecastday[1]
+        nextday = forecastday[2]
+        #put them in their own dict for easier iteration.
+        three_day = [today,tomorrow,nextday]
+
+        for x in three_day:
+            print(' '+'-'*5)
+            print('| {0:4}|'.format(x['date']['weekday_short']))
+
+if __name__ == '__main__':
+    extended_forecast()
