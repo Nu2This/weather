8b54bc3b639e3d95240dbe2bd14b00d0f34c870e
Michael Heidelberger <mheide@localhost.localdomain>
2018-03-28 16:25:14 -0500

    Corrected ArgParse to work correctly and connected it with the online api instead of reading from a file.

--- weather.py
+++ weather.py
@@ -1,28 +1,29 @@
 import json
 import argparse
+import requests
 
 
 def forecast(days):
-    with open('/home/mheide/Documents/python/Weather/56119.json') as json_file:
-        data = json.load(json_file)
-        #Dig into JSON data to grab what I want. 
-        quick = data['forecast']['simpleforecast']['forecastday']
-        text  = data['forecast']['txt_forecast']['forecastday']
-        for x in range(days):
-            print('{}\nTemp: {} to {}\nConditions: {}\nWind: {} - {} MPH\n'.format(
-                quick[x]['date']['weekday'],
-                quick[x]['low']['fahrenheit'],
-                quick[x]['high']['fahrenheit'],
-                quick[x]['conditions'],
-                quick[x]['avewind']['mph'],
-                quick[x]['maxwind']['mph']))
-        for x in range(days):
-            print('\n{}\n{}\n'.format(text[x]['title'],text[x]['fcttext']))
+    r = 'http://api.wunderground.com/api/08c90c66a7d001aa/forecast10day/q/MN/Worthington.json'
+    data = requests.get(r).json()
+    #Dig into JSON data to grab what I want. 
+    quick = data['forecast']['simpleforecast']['forecastday']
+    text  = data['forecast']['txt_forecast']['forecastday']
+    for x in range(days):
+        print('{}\nTemp: {} to {}\nConditions: {}\nWind: {} - {} MPH\n'.format(
+            quick[x]['date']['weekday'],
+            quick[x]['low']['fahrenheit'],
+            quick[x]['high']['fahrenheit'],
+            quick[x]['conditions'],
+            quick[x]['avewind']['mph'],
+            quick[x]['maxwind']['mph']))
+    for x in range(days):
+        print('\n{}\n{}\n'.format(text[x]['title'],text[x]['fcttext']))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('days', help="Number of days to look at", nargs = '?', const= 1, type = int, default = input('Enter number of Days: '))
+    parser.add_argument('days', help="Number of days to look at", nargs = '?', const = 3, type = int,default=3)
     args = parser.parse_args()
 
     forecast(args.days)
